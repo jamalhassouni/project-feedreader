bc9124fc5f4275aa416c9e4662c4050cb13a20bd
jamal hassouni <jamalhassouni13@gmail.com>
2018-07-02 17:18:03 +0100

    Improved code in RSS Feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,37 +31,27 @@ $(function () {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        function testEachFeedInallFeeds(feed) {
-            it('each feed has a URL defined and that the URL is not empty', function () {
-
-
+        it('each feed has a URL defined and that the URL is not empty', function () {
+            for (let feed of allFeeds) {
                 expect(feed.url).toBeDefined();
-                expect(typeof feed.url).toMatch("string");
-                expect(feed.url.trim().length).not.toBe(0);
-            });
-        }
-        for (var i = 0; i < allFeeds.length; i++) {
-            var feed = allFeeds[i];
-            testEachFeedInallFeeds(feed);
-        }
+                expect(feed.url.length).not.toEqual(0);
+            }
+        });
+
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        function testNameInAllFeeds(feed) {
             it('each feed has a name defined and that the name is not empty', function () {
-                expect(feed.name).toBeDefined();
-                expect(typeof feed.name).toMatch('string');
-                expect(feed.name.trim().length).not.toBe(0);
+                for (let feed of allFeeds) {
+                    expect(feed.name).toBeDefined();
+                    expect(feed.name.length).not.toEqual(0);
+                }
             });
-        }
-        var allFeedsLength = allFeeds.length;
-        for (var i = 0; i < allFeedsLength; i++) {
-            var feed = allFeeds[i];
-            testNameInAllFeeds(feed);
-        }
+
+
     });
 
 
